327d8542f2de75d17fc177594009594342b6910a
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-12-03 13:21:59 +0200

    fix intro img path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,7 +27,7 @@ const Home = ({ classicHeader, darkTheme, handleNavClick }) => {
         {/* ---------------image background------------------ */}
         <div
           className="hero-bg parallax"
-          style={{ backgroundImage: 'url("images/intro-bg.jpg")' }}
+          style={{ backgroundImage: 'url("/images/intro-bg.jpg")' }}
         ></div>
         <div className="hero-content section d-flex min-vh-100">
           <div className="container my-auto">
